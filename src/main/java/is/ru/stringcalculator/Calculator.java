bed0e301f14d8149ffdaeabe2f70fb76111d30ed
lara <lara15@ru.is>
2016-10-14 13:54:04 +0000

    lagadi villur i handle different delimiter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,13 +2,21 @@ package is.ru.stringcalculator;
 import java.util.ArrayList;
 
 public class Calculator {
-
 	public static int add(String text){
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
-			return sum(splitNumbers(text));
+		
+		String delimiter = ",";
+		// Check of custom delimiter
+		if (text.contains("//")){
+			String[] tmp = text.split("\\n");
+			delimiter = String.valueOf(text.charAt(2));
+			text = tmp[1];
+			return sum(splitNumbers(text, delimiter));
+		}
+		else if(text.contains(delimiter)){
+			return sum(splitNumbers(text, delimiter));
 		}
 		else
 			return 1;
@@ -17,15 +25,9 @@ public class Calculator {
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
+     
 
-	private static String[] splitNumbers(String numbers){
-		if(numbers.contains("\n")){
-			numbers = numbers.replace("\n", ",");
-		}
-	    return numbers.split(",");
-	}
-      
- private static int sum(String[] numbers){
+	private static int sum(String[] numbers){
 		int total = 0;
 		int n = 0; 
 		ArrayList negative = new ArrayList();
@@ -41,44 +43,23 @@ public class Calculator {
 			}
 		}
 
-		if(negative.size() < 0){
-			throw new RuntimeException("Negatives not allowed: " + negative.toString());
-		}
+		if(negative.size() > 0){
+			String allNegatives = negative.toString();
+			allNegatives = allNegatives.replace("[","");
+			allNegatives = allNegatives.replace("]","");
+			allNegatives = allNegatives.replace(" ","");
 
+			throw new RuntimeException("Negatives not allowed: " + allNegatives);
+		}
+		
 		return total;
 	}
 
-	/*public static int add(String text){
-		if(text.equals("")){
-			return 0;
-		}
-		
-		String delimeter = ",";
-		// Check of custom delimiter
-		if (text.contains("//")){
-			String [] tmp = text.split ("\\n");
-			delimeter = String.valueOf (text.charAt(2));
-			 text = tmp [1];
-			return splitNumbers (text, delimeter);
-
-		}
-		else if(text.contains(delimiter)){
-			return splitNumbers (text, delimeter);
-		}
-		else
-			return 1;
-	}
 	private static String[] splitNumbers(String numbers, String delimiter){
 	    if (numbers.contains("\n")){
 	    	numbers = numbers.replace("\n", delimiter);
 	    }
 	    return numbers.split(delimiter);
-	}*/
-
-
+	}
 }
-
-
-
-
 
